257f2c5278fae0991fee2e3e477d9ebeb4ffb28c
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-02-06 15:43:54 +0200

    Skeleton for the major part

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Options.Applicative
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
 
 import Options
 
+outputResult :: Either ByteString ByteString -> IO ()
+outputResult (Left errorMessage) = B.putStr "Error: " >> B.putStrLn errorMessage
+outputResult (Right message) = B.putStrLn message
+
+increaseSize :: Options -> IO (Either ByteString ByteString)
+increaseSize = undefined
+
 main :: IO ()
-main = execParser opts >>= undefined
+main = execParser opts >>= increaseSize >>= outputResult
   where opts = info (helper <*> optionsParser)
                     (fullDesc <> progDesc "Increase size of a PNG input file by SIZE bytes" 
                               <> header "super-size-png - a tool for increasing size of a PNG file")
