eb5ecff24b551e931065f0c40b73c1a287c622d6
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-02-06 14:06:11 +0200

    Parse command line options and view help message

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,9 @@
+import Options.Applicative
+
+import Options
+
 main :: IO ()
-main = undefined
+main = execParser opts >>= undefined
+  where opts = info (helper <*> optionsParser)
+                    (fullDesc <> progDesc "Increase size of a PNG input file by SIZE bytes" 
+                              <> header "super-size-png - a tool for increasing size of a PNG file")
