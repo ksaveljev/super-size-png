d301cce3839ac60375b2e29c1856070dd3d7943c
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2015-02-10 17:09:16 +0200

    Validate input file header

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Char (chr)
+import System.IO (openFile, IOMode(ReadMode))
 import Options.Applicative
 import Data.ByteString (ByteString)
-import Control.Error (runScript, hoistEither)
+import Control.Error (runScript, hoistEither, scriptIO)
 import qualified Data.ByteString.Char8 as B
 
 import Options
 
+pngHeaderSize :: Int
+pngHeaderSize = 8 -- bytes
+
+verifyPNGHeader :: ByteString -> Either String ()
+verifyPNGHeader h = if h == expectedHeader
+                      then Right () 
+                      else Left "Incorrect PNG file provided (header mismatch)"
+  where expectedHeader = B.pack . map chr $ [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]
+
 verifyOptions :: Options -> Either String ()
 verifyOptions options =
-    if sizeIncrease options < 10
+    if sizeIncrease options < 10 -- TODO: need to calculate what is the minimum size increase we can allow
       then Left "OH OH"
       else Right ()
+
+genereateTEXTChunk :: Options -> ByteString
+genereateTEXTChunk = undefined -- TODO
+
+appendChunkToFile :: Options -> ByteString -> IO ()
+appendChunkToFile = undefined -- TODO
 
 increasePNGSize :: Options -> IO ()
 increasePNGSize options = runScript $ do
   hoistEither $ verifyOptions options -- there is a limit on minimum size increase
-  undefined
-  {-
-  verifyInputFileHeader -- PNG header verification
-  genereateTEXTChunk -- bytestring with desired length
-  appendChunkToFile
-  -}
+  fileHeader <- scriptIO $ do
+    handle <- openFile (inputFile options) ReadMode
+    B.hGet handle pngHeaderSize
+  hoistEither $ verifyPNGHeader fileHeader
+  let textChunk = genereateTEXTChunk options
+  scriptIO $ appendChunkToFile options textChunk
+  return ()
 
 main :: IO ()
 main = execParser opts >>= increasePNGSize
